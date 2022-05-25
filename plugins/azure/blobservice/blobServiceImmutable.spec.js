fc703d77f8283421479dabe05d8eb30b628518b8
Mayank Bhola <mayankbhola@gmail.com>
2022-05-25 20:28:46 +0530

    Update blobServiceImmutable.spec.js

--- plugins/azure/blobservice/blobServiceImmutable.spec.js
+++ plugins/azure/blobservice/blobServiceImmutable.spec.js
@@ -214,10 +214,10 @@ describe('blobServiceImmutable', function() {
             });
         });
 
-        it('should give failing result if immutability is not configuredfor blob container', function(done) {
+        it('should [NOT] give failing result if immutability is not configuredfor blob container', function(done) {
             const cache = createCache([storageAccounts[0]], [blobServices[0]], [blobContainers[1]]);
             blobServiceImmutable.run(cache, {}, (err, results) => {
-                expect(results.length).to.equal(1);
+                expect(results.length).to.equal(2);
                 expect(results[0].status).to.equal(2);
                 expect(results[0].message).to.include('Immutability has not been configured for the blob container');
                 expect(results[0].region).to.equal('eastus');
